3e7e7e7bdfb50377d1727aa7910a43ba3f8cc25a
katzman <steve.r.katzman@gmail.com>
2024-01-30 19:20:08 +0000

    Added task to verify separately from deploy in case the block explorer cant see the newly deployed contracts yet

---
+++ tasks/verify.ts
@@ -0,0 +1,40 @@
+import { task } from "hardhat/config";
+import { logger } from "utils";
+import { isLocalNetwork, isProdNetwork } from "utils/networkHelpers";
+import { getAddresses, updateAddresses } from "utils/manageAddresses";
+import { deployLegitImpl, deployLegitAsProxy } from "utils/deployer";
+import { verify } from "utils/verify";
+import {Legit, Legit__factory, ProxyContract__factory} from "typechain-types";
+
+task("verify", "Will verify the contracts for the specified network")
+  .setAction(async (_ , hre) => {
+    try {
+      const addresses = await getAddresses(hre);
+      const [deployer, proxyAdmin] = await hre.ethers.getSigners();
+
+      const admin = proxyAdmin.address;
+      const tokens = [addresses.tokens.usdc, addresses.tokens.weth];
+
+      let legitImpl = Legit__factory.connect(addresses.LegitExchange.implementation, deployer);
+      await verify(hre, {
+          contract: legitImpl,
+          contractName: "Legit",
+      });
+
+      let legitProxy = ProxyContract__factory.connect(addresses.LegitExchange.proxy, deployer);
+      const initData = legitImpl.interface.encodeFunctionData("initialize", [
+        deployer.address,
+        addresses.uniswap,
+        tokens,
+      ]);
+
+      await verify(hre, {
+          constructorArguments: [legitImpl.address, admin, initData],
+          contract: legitProxy,
+          contractName: "ProxyContract",
+      });
+
+    } catch (error) {
+      logger.out(error, logger.Level.Error);
+    }
+  });
